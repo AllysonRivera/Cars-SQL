808dc6270f3fc53e46ec960cfd5e9ad8fd9870bc
AllysonRivera <53790752+AllysonRivera@users.noreply.github.com>
2024-08-18 16:46:38 -0700

    Update Car.sql

--- Car.sql
+++ Car.sql
@@ -1,1 +1,5 @@
+SELECT * FROM `project-1-432001.cars.car_info` LIMIT 1000;
 
+SELECT DISTINCT fuel_type FROM `project-1-432001.cars.car_info` LIMIT 1000;
+
+SELECT MIN(length) AS min_length,MAX(length) AS max_length FROM `project-1-432001.cars.car_info`;
